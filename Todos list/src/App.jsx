f62905cf480bd00d4ada382d9743502c539412e4
yashvsr <83863724+yashvsr@users.noreply.github.com>
2023-08-03 23:23:17 +0530

    Add files via upload

---
+++ Todos list/src/App.jsx
@@ -0,0 +1,18 @@
+
+import CssBaseline from "@mui/material/CssBaseline";
+import './App.css'
+import TodoList from './TodoList';
+import Navbar from "./Navbar";
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <CssBaseline />
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
